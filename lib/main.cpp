01f96e08d8a047531cb751d063f17370407cdae2
Pedro Chaidez <149631039+Pedro-Chaidez@users.noreply.github.com>
2026-02-26 13:40:20 -0800

    Setting up for the algorithm class

--- lib/main.cpp
+++ lib/main.cpp
@@ -1,8 +1,7 @@
-#include <iostream> //Input Output
-#include <fstream> //File Reading
-#include <vector> //Lists handling
-#include <exception> // Error handling
-#include <sstream> //Need for parsing strings into numbers
+#include <iostream>	 //Input Output
+#include <fstream>	 //File Reading
+#include <vector>		 //Lists handling
+#include <sstream>	 //Need for parsing strings into numbers
 #include "../Headers/algorithms.hpp"
 
 using namespace std;
@@ -10,18 +9,25 @@ using namespace std;
 int main()
 {
 	string inputString;
-	while(true){
-		cout<<"Welcome to Pedro Chaidez\'s Feature Selection Algorithm!"<<endl
-				<<"Type in the name of the file to test "<<endl
-				<<"(make sure all test files are in the Data folder as a txt file): ";
-		cin>>inputString;
-		cout<<endl<<endl;
+	NearestNeighborAlgorithm nn;
+
+	while (true)
+	{
+		cout << "Welcome to Pedro Chaidez\'s Feature Selection Algorithm!" << endl
+				 << "Type in the name of the file to test " << endl
+				 << "(make sure all test files are in the Data folder as a txt file): ";
+		cin >> inputString;
+		cout << endl << endl;
 
 		// Access the file
-		string file_path = "lib/Data/"+inputString;
+		string file_path = "lib/Data/" + inputString;
 		ifstream file(file_path);
-		if (!file.is_open()) throw runtime_error("Error: Could not open the file.\n");
-				
+		if (!file.is_open())
+		{
+			cout << "Error: Could not open the file " << file_path << endl;
+			continue;
+		}
+
 		// Read numbers from the file into a container
 		vector<vector<double>> data; // 2D vector to store rows and columns
 		string line;
@@ -55,40 +61,52 @@ int main()
 		}
 		file.close();
 
-		cout<<"Type the number of the algorithm you want to run."<<endl
-				<<"1) Forward Selection"<<endl
-				<<"2) Backward Elimination"<<endl
-				<<"Selection: ";
-		cin>>inputString;
-		cout << endl << endl
+		if (data.empty())
+		{
+			cout << "Error: File is empty or improperly formatted." << endl;
+			continue;
+		}
+
+		cout << "Type the number of the algorithm you want to run." << endl
+				 << "1) Forward Selection" << endl
+				 << "2) Backward Elimination" << endl
+				 << "Selection: ";
+
+		int selection;
+		cin >> selection;
+
+		cout << endl
+				 << endl
 				 << "This dataset has " << colCount - 1 << " features (not including the class attribute), with " << rowCount << " instances." << endl
-				 << endl
-				 << "Running nearest neighbor with all " << colCount - 1 << " features, using \"leaving-one-out\" evaluation, I get an accuracy of " << 0 << '%' << endl
-				 << endl
-				 << "Beginning search." << endl
 				 << endl;
 
-		// Use the numbers to print
-		cout << "Numbers read from file:" << endl;
-		for (vector<double> dataLine : data)
+		// Display initial search info as per the requested format
+		cout << "Beginning search." << endl
+				 << endl;
+
+		if (selection == FORWARD_SELECTION)
 		{
-			if(dataLine.size()>0)
-				cout<<"Class "<<dataLine.at(0)<<": ";
-			for(unsigned int i = 1; i<dataLine.size()-1;i++){
-				cout<<dataLine.at(i)<<", ";
-			}
-			if(dataLine.size()>1){
-				cout<<dataLine.at(dataLine.size()-1)<<endl;
-			}
+			nn.NNForwardSelectionSearch(data);
 		}
-		cout<<"Finished search! The best feature subset is {"<<0<<','<<0<<"}, which has an accuracy of "<<0<<'%'<<endl
-				<<endl
-				<<"Done with program? Type Quit"<<endl
-				<<"Anything else would restart the program..."<<endl
-				<<"Selection: ";
-		cin>>inputString;
-		cout<<endl<<endl;
-		if (inputString == "Quit") return 0;
+		else if (selection == BACKWARD_ELIMINATION)
+		{
+			nn.NNBackwardEliminationSearch(data);
+		}
+		else
+		{
+			cout << "Invalid selection. Returning to start..." << endl;
+			continue;
+		}
+
+		cout << endl
+				 << "Done with program? Type Quit" << endl
+				 << "Anything else would restart the program..." << endl
+				 << "Selection: ";
+		cin >> inputString;
+		cout << endl
+				 << endl;
+		if (inputString == "Quit" || inputString == "quit")
+			return 0;
 	}
 	return 0;
 }
